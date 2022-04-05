7b6626eedc4d6028cff8c0d4166bb736a15163f6
Evgenii Kalinovskii <olorin44@mail.ru>
2022-04-05 16:55:01 +0300

    initial project commit

---
+++ contracts/TicTacToe.sol
@@ -0,0 +1,347 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2;
+
+contract TicTacToe {
+    uint256 gamesCount = 0;
+
+    enum GameState {
+        Start,
+        Draw,
+        XPlaying,
+        OPlaying,
+        XVictory,
+        OVictory,
+        XVictoryByTimeout,
+        YVictoryByTimeOut
+    }
+
+    enum BoardMove {
+        None,
+        X,
+        O
+    }
+
+    struct TTTGame {
+        address player1;
+        address player2;
+        uint256 player1Bet;
+        uint256 player2Bet;
+        uint256 lastTimeStamp;
+        uint256 startTimeStamp;
+        GameState gameState;
+        BoardMove[3][3] board;
+    }
+
+    mapping(uint256 => TTTGame) private games;
+    mapping(address => uint256) private gameUserMappings;
+
+    event Player1Created(uint256 gameId, address player1, uint256 timeStamp);
+    event Player2Joined(
+        uint256 gameId,
+        TTTGame game,
+        address player2,
+        uint256 timeStamp
+    );
+    event Move(
+        uint256 gameId,
+        BoardMove[3][3] board,
+        uint256 timeStamp,
+        string moveType
+    );
+    event EndGame(
+        uint256 gameId,
+        TTTGame game,
+        uint256 timeStamp,
+        string endType
+    );
+
+    modifier checkUnoccupiedPresent(uint256 _gameId) {
+        require(
+            getUnoccupiedPresent(_gameId) == true,
+            "There is no space on the board to play"
+        );
+        _;
+    }
+
+    modifier checkCorrectBet() {
+        require(msg.value == 500000000000000000, "Invalid stake");
+        _;
+    }
+
+    modifier checkIsGameOwner(uint256 _gameId) {
+        require(gameUserMappings[msg.sender] == _gameId, "No right");
+        _;
+    }
+
+    function purgeGameData(uint256 _gameId) private {
+        delete gameUserMappings[games[_gameId].player1];
+        delete gameUserMappings[games[_gameId].player2];
+        delete games[_gameId];
+    }
+
+    function onlyUnenrolled() private view {
+        require(
+            gameUserMappings[msg.sender] == 0,
+            "You already play with someone"
+        );
+    }
+
+    /*function getBoard(uint256 _gameId)
+        public
+        view
+        returns (BoardMove[3][3] memory)
+    {
+        BoardMove[3][3] memory bm = games[_gameId].board;
+        return bm;
+    }*/
+
+    function getUnoccupiedPresent(uint256 _gameId) private view returns (bool) {
+        bool foundNone = false;
+
+        for (uint8 i; i < games[_gameId].board.length; i++) {
+            for (uint8 j; j < games[_gameId].board.length; j++) {
+                if (games[_gameId].board[i][j] == BoardMove.None) {
+                    foundNone = true;
+                    break;
+                }
+            }
+            if (foundNone == true) {
+                break;
+            }
+        }
+        return foundNone;
+    }
+
+    function getCurrentPlayer(uint256 _gameId) private view returns (address) {
+        if (games[_gameId].gameState == GameState.XPlaying) {
+            return games[_gameId].player1;
+        } else if (games[_gameId].gameState == GameState.OPlaying) {
+            return games[_gameId].player2;
+        } else {
+            revert("Unexpected");
+        }
+    }
+
+    function newGame() public payable checkCorrectBet returns (uint256) {
+        onlyUnenrolled();
+
+        require(gamesCount <= 200, "Many games");
+        gamesCount++;
+
+        gameUserMappings[msg.sender] = gamesCount;
+        games[gamesCount].player1 = msg.sender;
+        games[gamesCount].player1Bet = msg.value;
+        games[gamesCount].gameState = GameState.Start;
+        emit Player1Created(gamesCount, msg.sender, block.timestamp);
+
+        return gamesCount;
+    }
+
+    function joinGame(uint256 _gameId) public payable checkCorrectBet {
+        onlyUnenrolled();
+
+        require(games[_gameId].gameState == GameState.Start, "Players=2");
+
+        gameUserMappings[msg.sender] = _gameId;
+        games[_gameId].player2 = msg.sender;
+        games[_gameId].player2Bet = msg.value;
+
+        //initializing game fully
+        games[_gameId].startTimeStamp = block.timestamp;
+
+        games[_gameId].gameState = GameState.XPlaying;
+
+        emit Player2Joined(
+            _gameId,
+            games[_gameId],
+            msg.sender,
+            block.timestamp
+        );
+    }
+
+    function xPlay(
+        uint256 _gameId,
+        uint8 _i,
+        uint8 _j
+    ) public checkIsGameOwner(_gameId) {
+        require(games[_gameId].gameState == GameState.XPlaying, "O turn");
+        require(games[_gameId].board[_i][_j] == BoardMove.None, "Occupied");
+
+        games[_gameId].board[_i][_j] = BoardMove.X;
+        games[_gameId].gameState = GameState.OPlaying;
+        games[_gameId].lastTimeStamp = block.timestamp;
+
+        emit Move(_gameId, games[_gameId].board, block.timestamp, "X played");
+
+        checkWinner(_gameId);
+    }
+
+    function oPlay(
+        uint256 _gameId,
+        uint8 _i,
+        uint8 _j
+    ) public checkIsGameOwner(_gameId) {
+        require(games[_gameId].gameState == GameState.OPlaying, "X turn");
+        require(games[_gameId].board[_i][_j] == BoardMove.None, "Occupied");
+        games[_gameId].board[_i][_j] = BoardMove.O;
+        games[_gameId].gameState = GameState.XPlaying;
+        games[_gameId].lastTimeStamp = block.timestamp;
+
+        emit Move(_gameId, games[_gameId].board, block.timestamp, "O played");
+
+        checkWinner(_gameId);
+    }
+
+    function checkRows(uint256 _gameId)
+        private
+        view
+        returns (BoardMove moveType)
+    {
+        BoardMove[3][3] memory board = games[_gameId].board;
+        //1
+        if (
+            board[0][0] != BoardMove.None &&
+            board[0][0] == board[0][1] &&
+            board[0][0] == board[0][2]
+        ) {
+            return board[0][0];
+        }
+        //2
+        if (
+            board[1][0] != BoardMove.None &&
+            board[1][0] == board[1][1] &&
+            board[1][0] == board[1][2]
+        ) {
+            return board[1][0];
+        }
+        //3
+        if (
+            board[2][0] != BoardMove.None &&
+            board[2][0] == board[2][1] &&
+            board[2][0] == board[2][2]
+        ) {
+            return board[2][0];
+        }
+
+        //Checking columns
+
+        //1
+        if (
+            board[0][0] != BoardMove.None &&
+            board[0][0] == board[1][0] &&
+            board[0][0] == board[2][0]
+        ) {
+            return board[0][0];
+        }
+        //2
+        if (
+            board[0][1] != BoardMove.None &&
+            board[0][1] == board[1][1] &&
+            board[0][1] == board[2][1]
+        ) {
+            return board[0][1];
+        }
+        //3
+        if (
+            board[0][2] != BoardMove.None &&
+            board[0][2] == board[1][2] &&
+            board[0][2] == board[2][2]
+        ) {
+            return board[0][2];
+        }
+
+        //Checking diagonals
+
+        //1
+        if (
+            board[0][0] != BoardMove.None &&
+            board[0][0] == board[1][1] &&
+            board[0][0] == board[2][2]
+        ) {
+            return board[0][0];
+        }
+        //2
+        if (
+            board[0][2] != BoardMove.None &&
+            board[0][2] == board[1][1] &&
+            board[0][2] == board[2][0]
+        ) {
+            return board[0][2];
+        }
+
+        return BoardMove.None;
+    }
+
+    function checkWinner(uint256 _gameId) private {
+        // checking dimensions,
+        // if all the dimensions are full and the rows, diagonals or columns
+        // are not filled, draw.
+
+        BoardMove winBoardType = checkRows(_gameId);
+
+        //nobody found to win, checking for draw
+        if (winBoardType == BoardMove.None && !getUnoccupiedPresent(_gameId)) {
+            setDrawGame(_gameId);
+        } else if (winBoardType != BoardMove.None) {
+            setWinGame(_gameId, winBoardType);
+        }
+    }
+
+    function setDrawGame(uint256 _gameId) private {
+        games[_gameId].gameState = GameState.Draw;
+        payable(games[_gameId].player1).transfer(games[_gameId].player1Bet);
+        payable(games[_gameId].player2).transfer(games[_gameId].player2Bet);
+
+        emit EndGame(_gameId, games[_gameId], block.timestamp, "Draw");
+        purgeGameData(_gameId);
+    }
+
+    function claimWinGameByTimeout(uint256 _gameId)
+        public
+        checkIsGameOwner(_gameId)
+    {
+        //current player loses
+        require(
+            (block.timestamp - games[_gameId].lastTimeStamp) >= 300,
+            "Playing"
+        );
+
+        uint256 totalBet = games[_gameId].player1Bet +
+            games[_gameId].player2Bet;
+        address currentPlayer = getCurrentPlayer(_gameId);
+        if (games[_gameId].player1 == currentPlayer) {
+            games[_gameId].gameState = GameState.OVictory;
+            payable(games[_gameId].player2).transfer(totalBet);
+        } else {
+            games[_gameId].gameState = GameState.XVictory;
+            payable(games[_gameId].player1).transfer(totalBet);
+        }
+        emit EndGame(_gameId, games[_gameId], block.timestamp, "TimeOut");
+        purgeGameData(_gameId);
+    }
+
+    function setWinGame(uint256 _gameId, BoardMove _bm) private {
+        if (_bm == BoardMove.X) {
+            games[_gameId].gameState = GameState.XVictory;
+        } else if (_bm == BoardMove.O) {
+            games[_gameId].gameState = GameState.OVictory;
+        } else {
+            revert("Unexpected");
+        }
+
+        uint256 totalBet = games[_gameId].player1Bet +
+            games[_gameId].player2Bet;
+
+        if (games[_gameId].gameState == GameState.XVictory) {
+            // Player 1 is only X, so rewarding him.
+            payable(games[_gameId].player1).transfer(totalBet);
+        } else if (games[_gameId].gameState == GameState.OVictory) {
+            // Player 2 is only O, so rewarding him.
+            payable(games[_gameId].player2).transfer(totalBet);
+        }
+        emit EndGame(_gameId, games[_gameId], block.timestamp, "Win");
+        purgeGameData(_gameId);
+    }
+}
---
+++ test/TTT.js
@@ -0,0 +1,242 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+//helper function
+function logEvents(events) {
+	for (let k in events) {
+		//iterating events
+		let e = events[k].event;
+		let args = events[k].args;
+		console.log(e)
+		console.log(args);
+	}
+}
+
+describe('TicTacToe game creation testing:', function () {
+	let TTT, ttt, owner, addr1, addr2, addr3, addr4;
+
+	beforeEach(async () => {
+		TTT = await ethers.getContractFactory('TicTacToe');
+		ttt = await TTT.deploy();
+
+		[owner, addr1, addr2, addr3, addr4] = await ethers.getSigners();
+		await ttt.deployed();
+	});
+
+	it('Creating the game with an illegal stake is reverted', async () => {
+		const options = { value: ethers.utils.parseEther("0.4") }
+		await expect(ttt.connect(addr1).newGame(options)).to.be.revertedWith('Invalid stake');
+	});
+
+	it('Joining the game with an illegal stake is reverted', async () => {
+		const valid_options = { value: ethers.utils.parseEther("0.5") }
+		const invalid_options = { value: ethers.utils.parseEther("0.4") }
+		let tx = await ttt.connect(addr1).newGame(valid_options);
+		let rc = await tx.wait();
+
+		let gameId = 0;
+		if (rc.events.length > 0) {
+			gameId = rc.events[0].args.gameId;
+		}
+
+		await expect(ttt.connect(addr2).joinGame(gameId, invalid_options)).to.be.revertedWith('Invalid stake');
+	});
+
+	it('Initializing a game by an enrolled gamer(creator) is prohibited', async () => {
+		const options = { value: ethers.utils.parseEther("0.5") }
+		let tx = await ttt.connect(addr1).newGame(options);
+		let rc = await tx.wait();
+
+		await expect(ttt.connect(addr1).newGame(options)).to.be.reverted;
+	});
+
+	it('Initializing a game by an enrolled gamer(joinee) is prohibited', async () => {
+		const options = { value: ethers.utils.parseEther("0.5") }
+		let gameId = 0;
+
+		let tx = await ttt.connect(addr1).newGame(options);
+		let rc = await tx.wait();
+
+		if (rc.events.length > 0) {
+			gameId = rc.events[0].args.gameId;
+		}
+
+		let tx1 = await ttt.connect(addr2).joinGame(gameId, options);
+		await tx1.wait();
+
+		await expect(ttt.connect(addr2).newGame(options)).to.be.reverted;
+	});
+
+});
+
+describe('TicTacToe game mechanics:', function () {
+	let TTT, ttt, owner, addr1, addr2, gameId;
+	const options = { value: ethers.utils.parseEther("0.5") };
+
+	beforeEach(async () => {
+		TTT = await ethers.getContractFactory('TicTacToe');
+		ttt = await TTT.deploy();
+
+		[owner, addr1, addr2, addr3, addr4] = await ethers.getSigners();
+		await ttt.deployed();
+
+		let tx = await ttt.connect(addr1).newGame(options);
+		let rc = await tx.wait();
+
+		if (rc.events.length > 0) {
+			gameId = rc.events[0].args.gameId;
+		}
+
+		let tx1 = await ttt.connect(addr2).joinGame(gameId, options);
+		await tx1.wait();
+	});
+	it('Joining the game when there are 2 players', async () => {
+		await expect(ttt.connect(addr3).joinGame(gameId, options)).to.be.revertedWith('Players=2');
+	});
+	it('Accessing unauthorized game', async () => {
+		//Creating second game
+
+		let tx = await ttt.connect(addr3).newGame(options);
+		let rc = await tx.wait();
+
+		if (rc.events.length > 0) {
+			gameId = rc.events[0].args.gameId;
+		}
+
+		await expect(ttt.connect(addr1).xPlay(gameId, 1, 1)).to.be.revertedWith('No right');
+	});
+
+	it('Moves in a row are prohibited', async () => {
+		let tx = await ttt.connect(addr2).xPlay(gameId, 0, 1);
+		let rc = await tx.wait();
+
+		await expect(ttt.connect(addr2).xPlay(gameId, 1, 1)).to.be.reverted;
+	});
+	it('Moves in the same place are prohibited', async () => {
+		let tx = await ttt.connect(addr1).xPlay(gameId, 0, 1);
+		let rc = await tx.wait();
+
+		await expect(ttt.connect(addr2).oPlay(gameId, 0, 1)).to.be.reverted;
+	});
+	it('Game finishes when a column is filled', async () => {
+		let tx = await ttt.connect(addr1).xPlay(gameId, 0, 0);
+		let rc = await tx.wait();
+		// console.log("0");
+		// logEvents(rc.events);
+
+		let tx1 = await ttt.connect(addr2).oPlay(gameId, 0, 1);
+		let rc1 = await tx1.wait();
+		// console.log("1");
+		// logEvents(rc1.events);
+
+		let tx2 = await ttt.connect(addr1).xPlay(gameId, 1, 0);
+		let rc2 = await tx2.wait();
+		// console.log("2");
+		// logEvents(rc2.events);
+
+		let tx3 = await ttt.connect(addr2).oPlay(gameId, 1, 1);
+		let rc3 = await tx3.wait();
+		// console.log("3");
+		// logEvents(rc3.events);
+
+		let tx4 = await ttt.connect(addr1).xPlay(gameId, 2, 0);
+		let rc4 = await tx4.wait();
+		// console.log("4");
+		// logEvents(rc4.events);
+
+		//Game data is purged
+		await expect(ttt.connect(addr2).oPlay(gameId, 2, 1)).to.be.revertedWith('No right');
+	});
+	it('Game finishes when a row is filled', async () => {
+		let tx = await ttt.connect(addr1).xPlay(gameId, 0, 0);
+		let rc = await tx.wait();
+		// console.log("0");
+		// logEvents(rc.events);
+
+		let tx1 = await ttt.connect(addr2).oPlay(gameId, 1, 0);
+		let rc1 = await tx1.wait();
+		// console.log("1");
+		// logEvents(rc1.events);
+
+		let tx2 = await ttt.connect(addr1).xPlay(gameId, 0, 1);
+		let rc2 = await tx2.wait();
+		// console.log("2");
+		// logEvents(rc2.events);
+
+		let tx3 = await ttt.connect(addr2).oPlay(gameId, 1, 1);
+		let rc3 = await tx3.wait();
+		// console.log("3");
+		// logEvents(rc3.events);
+
+		let tx4 = await ttt.connect(addr1).xPlay(gameId, 0, 2);
+		let rc4 = await tx4.wait();
+		// console.log("4");
+		// logEvents(rc4.events);
+
+		//Game data is purged
+		await expect(ttt.connect(addr2).oPlay(gameId, 1, 2)).to.be.revertedWith('No right');
+	});
+	it('Game finishes when a diagonal is filled', async () => {
+		let tx = await ttt.connect(addr1).xPlay(gameId, 0, 0);
+		let rc = await tx.wait();
+		// console.log("0");
+		// logEvents(rc.events);
+
+		let tx1 = await ttt.connect(addr2).oPlay(gameId, 0, 1);
+		let rc1 = await tx1.wait();
+		// console.log("1");
+		// logEvents(rc1.events);
+
+		let tx2 = await ttt.connect(addr1).xPlay(gameId, 1, 1);
+		let rc2 = await tx2.wait();
+		// console.log("2");
+		// logEvents(rc2.events);
+
+		let tx3 = await ttt.connect(addr2).oPlay(gameId, 1, 2);
+		let rc3 = await tx3.wait();
+		// console.log("3");
+		// logEvents(rc3.events);
+
+		let tx4 = await ttt.connect(addr1).xPlay(gameId, 2, 2);
+		let rc4 = await tx4.wait();
+		// console.log("4");
+		// logEvents(rc4.events);
+
+		//Game data is purged
+		await expect(ttt.connect(addr2).oPlay(gameId, 0, 2)).to.be.revertedWith('No right');
+	});
+	it('Game finishes when draw', async () => {
+		let tx = await ttt.connect(addr1).xPlay(gameId, 0, 1);
+		let rc = await tx.wait();
+
+		let tx1 = await ttt.connect(addr2).oPlay(gameId, 0, 0);
+		let rc1 = await tx1.wait();
+
+		let tx2 = await ttt.connect(addr1).xPlay(gameId, 0, 2);
+		let rc2 = await tx2.wait();
+
+		let tx3 = await ttt.connect(addr2).oPlay(gameId, 1, 1);
+		let rc3 = await tx3.wait();
+
+		let tx4 = await ttt.connect(addr1).xPlay(gameId, 1, 0);
+		let rc4 = await tx4.wait();
+
+		let tx5 = await ttt.connect(addr2).oPlay(gameId, 1, 2);
+		let rc5 = await tx5.wait();
+
+		let tx6 = await ttt.connect(addr1).xPlay(gameId, 2, 1);
+		let rc6 = await tx6.wait();
+
+		let tx7 = await ttt.connect(addr2).oPlay(gameId, 2, 0);
+		let rc7 = await tx7.wait();
+
+		let tx8 = await ttt.connect(addr1).xPlay(gameId, 2, 2);
+		let rc8 = await tx8.wait();
+
+		// console.log("8");
+		// logEvents(rc8.events);
+
+		//Move + Win(Draw) events are fired
+		expect(rc8.events[1].args.endType).to.equal('Draw');
+	});
+});
